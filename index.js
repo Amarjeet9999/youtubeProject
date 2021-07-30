f767ef50356c1964287800f871762b77a1da7e0e
Amarjeet Kumar <amarjeetyt@gmail.com>
2021-07-30 13:03:51 +0530

    Adding Edited Files

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /** @format */
-let video_div = document.getElementById("mainVideoes");
+let video_div = document.getElementById("videoesYoutube");
 async function showVideoes() {
-  let q = "swanand kadam";
+  let q = "google";
   let res = await fetch(
-    `https://youtube.googleapis.com/youtube/v3/search?q=${q}&key=AIzaSyC7DLjrFfkeFkFJn5ITjrlKTIzI-d3nFmc&maxResults=25`
+    `https://youtube.googleapis.com/youtube/v3/search?q=${q}&key=AIzaSyC7DLjrFfkeFkFJn5ITjrlKTIzI-d3nFmc&maxResults=25&order=viewcount`
   );
   let data = await res.json();
   //
@@ -15,9 +15,8 @@ async function showVideoes() {
   items.forEach(({ id: { videoId } }) => {
     let div = document.createElement("div");
     div.setAttribute("id", "divVideoes");
-    div.style.marginTop = "40px";
     div.style.margin = "auto";
-    div.style.marginTop = "40px";
+    div.style.marginTop = "15px";
     div.innerHTML = `<iframe width="280" height="160" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 
     video_div.appendChild(div);
@@ -25,13 +24,38 @@ async function showVideoes() {
 }
 showVideoes();
 
-// let mainDivVideo = document.getElementById("mainVideoes");
+//For search bar
+let video = document.getElementById("videoesYoutube");
+async function findVideoes() {
+  video.innerHTML = null;
+  let q = document.getElementById("search").value;
+  let res = await fetch(
+    `https://youtube.googleapis.com/youtube/v3/search?q=${q}&key=AIzaSyC7DLjrFfkeFkFJn5ITjrlKTIzI-d3nFmc&maxResults=20`
+  );
+  let data = await res.json();
+  let { items } = data;
+  items = items.filter((el) => {
+    return el.id.videoId != undefined;
+  });
+  items.forEach(({ id: { videoId } }) => {
+    console.log(videoId);
 
-// async function findVideoes() {
-//   mainDivVideo.innerHTML = null;
+    let div = document.createElement("div");
+    div.style.margin = "auto";
+    div.style.marginTop = "15px";
+    div.innerHTML = `<iframe width="280" height="160" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
+    video.appendChild(div);
+  });
+}
+
+//For selector
+// let videoOnSelector = document.getElementById("videoesYoutube");
+// async function findVideoesOnSelector() {
+//   video.innerHTML = null;
 //   let q = document.getElementById("search").value;
 //   let res = await fetch(
-//     `https://youtube.googleapis.com/youtube/v3/search?q=${q}&key=AIzaSyBdXe5uB4AZ3-tUWvlRayH4rvgNgMbY1cg&maxResults=15`
+//     `https://youtube.googleapis.com/youtube/v3/search?q=${q}&key=AIzaSyCMwhDVtzPB9hBVEusVhzudOQIynX8iOuI&maxResults=20`
 //   );
 //   let data = await res.json();
 //   let { items } = data;
@@ -42,12 +66,14 @@ showVideoes();
 //     console.log(videoId);
 
 //     let div = document.createElement("div");
-//     div.setAttribute("id", "divVideoes");
-//     div.style.marginTop = "40px";
 //     div.style.margin = "auto";
-//     div.style.marginTop = "40px";
+//     div.style.marginTop = "15px";
 //     div.innerHTML = `<iframe width="280" height="160" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 
-//     mainDivVideo.appendChild(div);
+//     videoOnSelector.appendChild(div);
 //   });
 // }
+
+function home() {
+  window.location.href = "index.html";
+}
